941b5f192cc4ec55f9f1343e348006bc6b85c477
andershup <anders242@gmail.com>
2020-07-21 13:30:55 +0000

    editrecipe.html set with default values. Update route set up and linked. Recipes updating correctly.

--- app.py
+++ app.py
@@ -14,35 +14,52 @@ app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 print(os.getenv('MONGO_URI'))
 mongo = PyMongo(app)
 
+
 @app.route("/")
-def get_recipes():
-    recipes=mongo.db.recipes.find(),
-    categories=mongo.db.categories.find(),
+def show_index():
     return render_template("index.html")
 
 
 @app.route('/addrecipe')
 def add_recipe():
     return render_template("addrecipe.html", categories=mongo.db.categories.find())
-    
+
+
 @app.route('/insertrecipe', methods=['POST'])
 def insert_recipe():
-    recipes=mongo.db.recipes
+    recipes = mongo.db.recipes
     recipes.insert_one(request.form.to_dict())
-    return redirect(url_for('show_recipe')) 
-     # only redirected to show_recipe for test purposes 
+    return redirect(url_for('recipe_list'))
 
-
-
-@app.route('/editrecipe/ <recipe_id>')
-def edit_recipe(recipe_id):
-    the_recipe=mongo.db.recipes.find_one({"_id: ObjectId(recipe_id)"})
-    all_categories=mongo.db.categories.find()
-    return render_template('editrecipe.html', recipe=the_recipe , categories=all_categories)
 
 @app.route('/recipelist')
 def recipe_list():
     return render_template("recipelist.html", recipes=mongo.db.recipes.find())
+
+
+@app.route('/editrecipe/ <recipe_id>')
+def edit_recipe(recipe_id):
+    the_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    all_categories = mongo.db.categories.find()
+    return render_template('editrecipe.html', recipe=the_recipe, categories=all_categories)
+
+
+@app.route('/updaterecipe/ <recipe_id>', methods=["POST"])
+def update_recipe(recipe_id):
+    recipes = mongo.db.recipes
+    recipes.update({'_id': ObjectId(recipe_id)},
+                   {
+        'recipe_name': request.form.get('recipe_name'),
+        'category_name': request.form.get('category_name'),
+        'added_by': request.form.get('added_by'),
+        'serves': request.form.get('serves'),
+        'prep_time': request.form.get('prep_time'),
+        'cooking_time': request.form.get('cooking_time'),
+        'prep_time': request.form.get('prep_time'),
+        'ingredients': request.form.get('ingredients'),
+        'instructions': request.form.get('instructions')
+    })
+    return redirect(url_for('recipe_list'))
 
 @app.route('/deleterecipe/ <recipe_id>')
 def delete_recipe(recipe_id):
@@ -50,10 +67,7 @@ def delete_recipe(recipe_id):
     return redirect(url_for('recipe_list'))
 
 
-
-
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
-
